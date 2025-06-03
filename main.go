855babe47fe32dc8ebd6c125d7cf0bb948ba4e0e
anatolio-deb <nikiforova693@gmail.com>
2025-06-03 12:18:09 +0000

    use https api

--- main.go
+++ main.go
@@ -128,11 +128,10 @@ func main() {
 	r.POST("/api/auth", showInitDataMiddleware)
 	// r.POST("/api/users", UserAdd)
 
-	err := r.Run(":8080")
+	// err := r.Run(":8080")
 
-	// err := r.RunTLS(":443", "/etc/letsencrypt/live/picovpn.ru/fullchain.pem", "/etc/letsencrypt/live/picovpn.ru/privkey.pem")
+	err := r.RunTLS(":8080", "/etc/letsencrypt/live/picovpn.ru/fullchain.pem", "/etc/letsencrypt/live/picovpn.ru/privkey.pem")
 	if err != nil {
 		panic(err)
 	}
-
 }
