9c9da48fc4caf77d2c31a2cc6182b1b18e8c6c7f
anatolio-deb <nikiforova693@gmail.com>
2025-06-08 22:43:11 +0000

    refactor

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/gin-contrib/cors"
@@ -9,101 +11,186 @@ import (
 	initdata "github.com/telegram-mini-apps/init-data-golang"
 )
 
-var TOKEN string
+// var TOKEN string
 
-func init() {
-	// Get token from environment variable.
-	TOKEN = os.Getenv("TELEGRAM_BOT_TOKEN")
+// func init() {
+// 	// Get token from environment variable.
+// 	TOKEN = os.Getenv("TELEGRAM_BOT_TOKEN")
+// }
+
+type contextKey string
+
+const (
+	_initDataKey contextKey = "init-data"
+)
+
+// Returns new context with specified init data.
+func withInitData(ctx context.Context, initData initdata.InitData) context.Context {
+	return context.WithValue(ctx, _initDataKey, initData)
 }
 
-func Validate(context *gin.Context) {
-	// Init data in raw format.
-	initDataRaw := context.GetHeader("X-Telegram-Data")
+// Returns the init data from the specified context.
+func ctxInitData(ctx context.Context) (initdata.InitData, bool) {
+	initData, ok := ctx.Value(_initDataKey).(initdata.InitData)
+	return initData, ok
+}
 
-	// Define how long since init data generation date init data is valid.
-	expIn := 24 * time.Hour
-
-	// Will return error in case, init data is invalid.
-	err := initdata.Validate(initDataRaw, TOKEN, expIn)
-	if err != nil {
-		context.AbortWithStatusJSON(401, map[string]any{
-			"message": "Unauthorized",
-		})
-		return
-	}
-	// If init data is valid, you can parse it.
-	initData, err := initdata.Parse(initDataRaw)
-	if err != nil {
-		context.AbortWithStatusJSON(401, map[string]any{
-			"message": "Unauthorized",
-		})
-		return
-	}
-	// If init data is valid, you can use it.
-	user, err := UserGetByTelegramID(initData.User.ID)
-	if err != nil {
-		context.String(500, err.Error())
-		return
-	}
-	// If user is not found, you can create a new one.
-	if user == nil {
-		user = &User{
-			// PlanID:     plan.ID,
-			// Plan:       plan,
-			TelegramID:       initData.User.ID,
-			ChatID:           initData.Chat.ID,
-			TelegramUsername: initData.User.Username,
-		}
-		// Save user to database.
-		result := DB.Create(&user)
-		// If there is an error while saving user to database, return error.
-		if result.Error != nil {
-			context.AbortWithStatusJSON(500, map[string]any{
-				"message": "Internal Server Error",
+// Middleware which authorizes the external client.
+func authMiddleware(token string) gin.HandlerFunc {
+	return func(context *gin.Context) {
+		// We expect passing init data in the Authorization header in the following format:
+		// <auth-type> <auth-data>
+		// <auth-type> must be "tma", and <auth-data> is Telegram Mini Apps init data.
+		authParts := strings.Split(context.GetHeader("authorization"), " ")
+		if len(authParts) != 2 {
+			context.AbortWithStatusJSON(401, map[string]any{
+				"message": "Unauthorized",
 			})
 			return
 		}
+
+		authType := authParts[0]
+		authData := authParts[1]
+
+		switch authType {
+		case "X-Telegram-Data":
+			// Validate init data. We consider init data sign valid for 1 hour from their
+			// creation moment.
+			if err := initdata.Validate(authData, token, time.Hour); err != nil {
+				context.AbortWithStatusJSON(401, map[string]any{
+					"message": err.Error(),
+				})
+				return
+			}
+
+			// Parse init data. We will surely need it in the future.
+			initData, err := initdata.Parse(authData)
+			if err != nil {
+				context.AbortWithStatusJSON(500, map[string]any{
+					"message": err.Error(),
+				})
+				return
+			}
+
+			context.Request = context.Request.WithContext(
+				withInitData(context.Request.Context(), initData),
+			)
+		}
 	}
-	// If you want to update user data, you can do it here.
-	context.JSON(200, user)
 }
 
+// Middleware which shows the user init data.
+func showInitDataMiddleware(context *gin.Context) {
+	initData, ok := ctxInitData(context.Request.Context())
+	if !ok {
+		context.AbortWithStatusJSON(401, map[string]any{
+			"message": "Init data not found",
+		})
+		return
+	}
+
+	context.JSON(200, initData)
+}
+
+// func main() {
+//
+
+// 	r := gin.New()
+
+// 	if err := r.Run(":3000"); err != nil {
+// 		panic(err)
+// 	}
+// }
+
+// func Validate(context *gin.Context) {
+// 	// Init data in raw format.
+// 	initDataRaw := context.GetHeader("X-Telegram-Data")
+
+// 	// Define how long since init data generation date init data is valid.
+// 	expIn := 24 * time.Hour
+
+// 	// Will return error in case, init data is invalid.
+// 	err := initdata.Validate(initDataRaw, TOKEN, expIn)
+// 	if err != nil {
+// 		context.AbortWithStatusJSON(401, map[string]any{
+// 			"message": "Unauthorized",
+// 		})
+// 		return
+// 	}
+// 	// If init data is valid, you can parse it.
+// 	initData, err := initdata.Parse(initDataRaw)
+// 	if err != nil {
+// 		context.AbortWithStatusJSON(401, map[string]any{
+// 			"message": "Unauthorized",
+// 		})
+// 		return
+// 	}
+// 	// If init data is valid, you can use it.
+// 	user, err := UserGetByTelegramID(initData.User.ID)
+// 	if err != nil {
+// 		context.String(500, err.Error())
+// 		return
+// 	}
+// 	// If user is not found, you can create a new one.
+// 	if user == nil {
+// 		user = &User{
+// 			// PlanID:     plan.ID,
+// 			// Plan:       plan,
+// 			TelegramID:       initData.User.ID,
+// 			ChatID:           initData.Chat.ID,
+// 			TelegramUsername: initData.User.Username,
+// 		}
+// 		// Save user to database.
+// 		result := DB.Create(&user)
+// 		// If there is an error while saving user to database, return error.
+// 		if result.Error != nil {
+// 			context.AbortWithStatusJSON(500, map[string]any{
+// 				"message": "Internal Server Error",
+// 			})
+// 			return
+// 		}
+// 	}
+// 	// If you want to update user data, you can do it here.
+// 	context.JSON(200, user)
+// }
+
 func main() {
+	// Your secret bot token.
+	token := os.Getenv("TELEGRAM_BOT_TOKEN")
 	// Set gin mode to release.
 	r := gin.New()
+
 	gin.SetMode(gin.ReleaseMode)
 	// Set gin logger.
-	r.Use(gin.LoggerWithConfig(gin.LoggerConfig{
-		Formatter: func(param gin.LogFormatterParams) string {
-			return "[" + param.TimeStamp.Format(time.RFC3339) + "] " +
-				param.Method + " " +
-				param.Path + " " +
-				param.ClientIP + " " +
-				param.ErrorMessage + " " +
-				param.Latency.String() + "\n"
-		},
-		Output: os.Stdout,
-	}))
-	// Set gin recovery.
-	r.Use(gin.Recovery())
-	// Set CORS middleware.
-	// You can customize CORS settings here.
-	// For example, you can allow only specific origins, methods, headers, etc
-	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"https://picovpn.ru", "https://www.picovpn.ru", "https://picovpn.ru:8080", "https://www.picovpn.ru:8080"},
-		AllowMethods:     []string{"PUT", "PATCH", "POST", "GET", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "X-Requested-With", "X-Telegram-Data", "Authorization"},
-		ExposeHeaders:    []string{"Content-Length", "X-Total-Count"},
-		AllowCredentials: true,
-		AllowOriginFunc: func(origin string) bool {
-			return origin == "https://picovpn.ru"
-		},
-		MaxAge: 12 * time.Hour,
-	}))
-	r.POST("/api/auth", Validate)
+
+	r.Use(
+		authMiddleware(token),
+		gin.LoggerWithConfig(gin.LoggerConfig{
+			Formatter: func(param gin.LogFormatterParams) string {
+				return "[" + param.TimeStamp.Format(time.RFC3339) + "] " +
+					param.Method + " " +
+					param.Path + " " +
+					param.ClientIP + " " +
+					param.ErrorMessage + " " +
+					param.Latency.String() + "\n"
+			},
+			Output: os.Stdout,
+		}),
+		cors.New(cors.Config{
+			AllowOrigins:     []string{"https://picovpn.ru", "https://www.picovpn.ru", "https://picovpn.ru:8080", "https://www.picovpn.ru:8080"},
+			AllowMethods:     []string{"PUT", "PATCH", "POST", "GET", "DELETE", "OPTIONS"},
+			AllowHeaders:     []string{"Origin", "Content-Type", "X-Requested-With", "X-Telegram-Data", "Authorization"},
+			ExposeHeaders:    []string{"Content-Length", "X-Total-Count"},
+			AllowCredentials: true,
+			AllowOriginFunc: func(origin string) bool {
+				return origin == "https://picovpn.ru"
+			},
+			MaxAge: 12 * time.Hour,
+		}),
+		gin.Recovery())
+	r.POST("/api/auth", showInitDataMiddleware)
 	// r.POST("/api/users", UserAdd)
 
-	// err := r.Run(":8080")
 	// Run the server on port 8080 with TLS.
 	// Make sure to replace the paths to your SSL certificate and key files.
 	// You can use Let's Encrypt or any other certificate authority.
